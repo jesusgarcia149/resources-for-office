d1c189efe6425d9deecaf1785ef04de79f55931b
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-08 11:13:00 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,14 @@
 powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 choco install googlechrome -y
-choco install vlc -y
-choco install qbittorrent -y
 choco install libreoffice -y
+choco install naps2 -y
 choco install sumatrapdf -y
+
 choco install anydesk -y
 choco install teamviewer -y
 choco install wget -y
+
+wget https://mega.nz/file/DaxUAALY#z_Udxp3seghUdfM8sueovfsHExqCp0LIxhIhHLMQ6GA
 wget https://github.com/hellzerg/optimizer/releases/download/15.6/Optimizer-15.6.exe
 wget https://www.3dpchip.com/new/3DP_Chip_v2307.exe
