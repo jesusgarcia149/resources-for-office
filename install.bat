33a517b6d9d3888bc909f02197bcc66dbbe03728
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-09-09 10:05:29 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,15 +2,9 @@ powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.Se
 
 choco install googlechrome -y
 choco install libreoffice -y
-choco install naps2 -y
 choco install sumatrapdf -y
-choco install qalculate -y
+choco install naps2 -y
 
 choco install megasync -y
 choco install anydesk -y
 choco install teamviewer -y
-choco install wget -y
-
-wget https://mega.nz/file/DaxUAALY#z_Udxp3seghUdfM8sueovfsHExqCp0LIxhIhHLMQ6GA
-wget https://github.com/hellzerg/optimizer/releases/download/15.6/Optimizer-15.6.exe
-wget https://www.3dpchip.com/new/3DP_Chip_v2307.exe
