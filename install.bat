fe29fc5a44ada2d703e44689f82675d1b28e2ed7
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-11-08 12:46:03 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -9,7 +9,8 @@ choco install googledrive -y
 choco install anydesk -y
 choco install teamviewer -y
 
+choco install omnifyhotspot -y
+
 choco install wget -y
 wget https://desktop-release.canva.com/Canva%20Setup%201.75.0.exe
 wget https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/capcut_capcutpc_0_1.2.2_downloader.exe
-wget https://downloads-direct.connectify.me/ConnectifyInstaller.exe
