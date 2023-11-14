bc4c05a91e3b8a5f378c62595ceeb42cd3c83016
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2023-11-13 21:52:49 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 choco install googlechrome -y
-choco install libreoffice -y
 choco install thunderbird -y
+choco install libreoffice -y
 choco install sumatrapdf -y
 choco install naps2 -y
 
