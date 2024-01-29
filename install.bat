fdf294c2b6c642e36dfb284fd39f2f58b4301d80
Jesus Garcia <35648860+jesusgarcia149@users.noreply.github.com>
2024-01-29 10:53:53 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,18 +1,18 @@
 powershell.exe "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 
 choco install googlechrome -y
-choco install thunderbird -y
+# choco install thunderbird -y
 choco install libreoffice -y
-choco install sumatrapdf -y
+# choco install sumatrapdf -y
 choco install naps2 -y
 
 choco install googledrive -y
-choco install anydesk -y
-choco install teamviewer -y
+# choco install anydesk -y
+# choco install teamviewer -y
 
-choco install omnifyhotspot -y
+# choco install omnifyhotspot -y
 
 choco install wget -y
-wget https://desktop-release.canva.com/Canva%20Setup%201.75.0.exe
-wget https://static.remove.bg/download/removebg-for-windows-2.0.1.exe
+# wget https://desktop-release.canva.com/Canva%20Setup%201.75.0.exe
+# wget https://static.remove.bg/download/removebg-for-windows-2.0.1.exe
 wget https://lf16-capcut.faceulv.com/obj/capcutpc-packages-us/installer/capcut_capcutpc_0_1.2.2_downloader.exe
